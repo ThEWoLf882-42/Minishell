cf0c5c52d29c58e76cd89ab7bb049d2474b889b1
ThEWoLf882-42 <agimi@student.1337.ma>
2023-05-21 22:57:47 +0100

    try to clean code

--- clean_lin.c
+++ clean_lin.c
@@ -6,11 +6,24 @@
 /*   By: agimi <agimi@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 21:35:08 by agimi             #+#    #+#             */
-/*   Updated: 2023/05/21 22:49:06 by agimi            ###   ########.fr       */
+/*   Updated: 2023/05/21 22:54:10 by agimi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	sq_clean(t_line *lv, char *line, int *i, int *j)
+{
+	line = malloc(sizeof(char) * (ft_strlen(lv->shx) + 1));
+	*j = -1;
+	while (lv->shx[++(*i)])
+	{
+		if (lv->shx[*i] != '\'')
+			line[++(*j)] = lv->shx[*i];
+	}
+	free(lv->shx);
+	lv->shx = line;
+}
 
 void	clean_lin(void)
 {
@@ -28,17 +41,7 @@ void	clean_lin(void)
 		{
 			i = -1;
 			if (ft_strchr(lv->shx, '\''))
-			{
-				line = malloc(sizeof(char) * (ft_strlen(lv->shx) + 1));
-				j = -1;
-				while (lv->shx[++i])
-				{
-					if (lv->shx[i] != '\'')
-						line[++j] = lv->shx[i];
-				}
-				free(lv->shx);
-				lv->shx = line;
-			}
+				sq_clean(lv, line, i, j);
 			lv = lv->nxt;
 		}
 		pv = pv->nxt;
