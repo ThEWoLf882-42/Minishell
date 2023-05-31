a11e671b1a63a611781f62aaf1d4f1b196d00ca0
ThEWoLf882-42 <agimi@student.1337.ma>
2023-05-31 22:42:23 +0100

    return the fill_sp to  def

--- fill_sp.c
+++ fill_sp.c
@@ -6,44 +6,19 @@
 /*   By: agimi <agimi@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 16:16:25 by agimi             #+#    #+#             */
-/*   Updated: 2023/05/31 22:08:08 by agimi            ###   ########.fr       */
+/*   Updated: 2023/05/31 22:41:20 by agimi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	loop_sp(char *line, char *pl)
-{
-	int		i;
-	int		j;
-	int		q;
-
-	i = 0;
-	q = 0;
-	while (line[i])
-	{
-		if (line[i] == '"' || line[i] == '\'')
-			q = !q;
-		else if (line[i] == '|' && !q)
-		{
-			pl[j] = '\0';
-			ft_backpipe(&g_va.sp, new_sp(pl));
-			j = -1;
-		}
-		else
-			pl[j] = line[i];
-		i++;
-		j++;
-	}
-	pl[j] = '\0';
-	ft_backpipe(&g_va.sp, new_sp(pl));
-}
-
-
 int	fill_sp(char *line)
 {
 	char	*pl;
+	int		i;
+	int		j;
 
+	i = -1;
 	if (!*line)
 		return (1);
 	if (line[ft_strlen(line) - 1] == '|')
@@ -51,7 +26,16 @@ int	fill_sp(char *line)
 	pl = malloc(sizeof(char) * ft_strlen(line) + 1);
 	if (!pl)
 		return (1);
-	loop_sp(line, pl);
+	while (line[++i])
+	{
+		j = -1;
+		while (line[i] && line[i] != '|')
+			pl[++j] = line[i++];
+		pl[++j] = '\0';
+		ft_backpipe(&g_va.sp, new_sp(pl));
+		if (line[i] == '\0')
+			break ;
+	}
 	free(pl);
 	return (0);
 }
