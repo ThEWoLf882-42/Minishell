f60a6d6ae34d781030359a33ba3c8f788b041536
ThEWoLf882-42 <agimi@student.1337.ma>
2023-06-09 15:06:48 +0100

    system leak rm

--- minishell.c
+++ minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zouaraqa <zouaraqa@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: agimi <agimi@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 15:25:26 by agimi             #+#    #+#             */
-/*   Updated: 2023/06/07 20:18:33 by zouaraqa         ###   ########.fr       */
+/*   Updated: 2023/06/09 15:06:20 by agimi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,6 @@ int	fill_check(void)
 		if (builtins(g_va.sp->lin, 0))
 		{
 			ft_free();
-			system("leaks minishell");// rm
 			return (1);
 		}
 	}
@@ -75,16 +74,15 @@ int	main(int ac, char **av, char **env)
 	{
 		g_va.line = readline("minishell-69$ ");
 		add_history(g_va.line);
-		if (!*g_va.line)
-			free(g_va.line);
 		if (!g_va.line)
 			exit_cmd(NULL, 0);
+		if (!*g_va.line)
+			free(g_va.line);
 		if (fill_check())
 			continue ;
 		forking();
 		wait_pid();
 		ft_free();
 		g_va.y = 0;
-		system("leaks minishell");//rm
 	}
 }
