ae6fdf9725550955f45d87444d3cf0cb7e9f27b7
ThEWoLf882-42 <agimi@student.1337.ma>
2023-06-12 12:58:20 +0100

    sig == 0 to !

--- sig_handel.c
+++ sig_handel.c
@@ -6,7 +6,7 @@
 /*   By: agimi <agimi@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 22:49:35 by agimi             #+#    #+#             */
-/*   Updated: 2023/06/09 15:22:06 by agimi            ###   ########.fr       */
+/*   Updated: 2023/06/10 13:50:49 by agimi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	ha_sig(int sig)
 	}
 	if (sig == SIGINT)
 	{
-		if (g_va.x == 0)
+		if (!g_va.x)
 		{
 			rl_replace_line("", 0);
 			printf("\n");
